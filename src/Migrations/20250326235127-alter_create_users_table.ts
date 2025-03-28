9f1bc7c345aae2457c8c48f418a2aa788c06d87b
DESKTOP-BKUA5T1\Nafis1053 <nafisiqbal10053@gmail.com>
2025-03-28 23:04:25 +0600

    #Fixed migration files. Table name started with uppercase letter.

--- src/Migrations/20250326235127-alter_create_users_table.ts
+++ src/Migrations/20250326235127-alter_create_users_table.ts
@@ -4,13 +4,13 @@ import { QueryInterface, Sequelize, DataTypes } from 'sequelize';
 
 module.exports = {
   async up (queryInterface: QueryInterface, Sequelize: Sequelize) {
-    await queryInterface.changeColumn('Users', 'rating', {
+    await queryInterface.changeColumn('users', 'rating', {
       type: DataTypes.FLOAT,
       allowNull: false,
       defaultValue: 0.0,
     });
 
-    await queryInterface.changeColumn('Users', 'credits', {
+    await queryInterface.changeColumn('users', 'credits', {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 100.0,
@@ -18,12 +18,12 @@ module.exports = {
   },
 
   async down (queryInterface: QueryInterface, Sequelize: Sequelize) {
-    await queryInterface.changeColumn('Users', 'rating', {
+    await queryInterface.changeColumn('users', 'rating', {
       type: DataTypes.FLOAT,
       allowNull: false,
     });
 
-    await queryInterface.changeColumn('Users', 'credits', {
+    await queryInterface.changeColumn('users', 'credits', {
       type: DataTypes.INTEGER,
       allowNull: false,
     });
